0dfd3d1979128d8d5f5d4bb40f65e826a3f8573d
GrverAbdul <groverabdul@gmail.com>
2026-05-26 22:33:23 -0400

    envio

--- blueprintapp/app.py
+++ blueprintapp/app.py
@@ -1,27 +1,43 @@
 # blueprintapp/app.py
 # fabrica de aplicaciones flask
-from flask import Flask
+
+from flask import Flask, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask, render_template
+
 # instancia global de la base de datos
 db = SQLAlchemy()
+
 # manejador de migraciones
 migrate = Migrate()
+
 def create_app():
+
     # crear aplicacion flask
     app = Flask(__name__, template_folder='templates')
+
     # configuracion sqlite
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inv_ventas.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     # iniciar extensiones
     db.init_app(app)
     migrate.init_app(app, db)
-    # importar blueprints (desde sus rutas)
+
+    # importar blueprints
     from blueprintapp.clientes.routes import bp_clientes
     from blueprintapp.productos.routes import bp_productos
     from blueprintapp.pedidos.routes import bp_pedidos
-    # registrar blueprints con prefijos
+
+    # registrar blueprints
     app.register_blueprint(bp_clientes, url_prefix="/clientes")
     app.register_blueprint(bp_productos, url_prefix="/productos")
     app.register_blueprint(bp_pedidos, url_prefix="/pedidos")
+
+    # ✅ RUTA RAÍZ (ESTO SOLUCIONA TODO)
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+
     return app
